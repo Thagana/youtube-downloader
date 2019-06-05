1aaff2da703e5ad9b50fcb6d803c93590b801771
thagana <samuelmthwa79@gmail.com>
2019-06-05 11:52:25 -0700

    main appliaction

--- application/app.py
+++ application/app.py
@@ -3,23 +3,20 @@ from pytube import YouTube, Playlist
 import click
 import os
 
+@click.group()
+def main():
+	"""  	CMDownloader \n
+	***** VERSION 0.0.1 ***** \n
+		Download YouTube Videos on The termial  \n
 
-@click.command()
-@click.option('--url', prompt='Enter YouTube Video URL')
-@click.option('--playlist', prompt='Playlis(y/n)') #playlist options
-@click.option('--directory', prompt='Folder Name')
-def download_vid(url, playlist, directory):
+	"""
+	pass
+
+@main.command()
+@click.argument('url')
+def vid(url):
 	try:
-
-		if playlist == 'n':
-			tube = YouTube(url).streams.first().download()
-		elif playlist == 'y':
-			createFolder(directory)
-			tube = Playlist(url).download_all(directory)
-		else:
-			click.echo('Error Unspecified option')
-
-
+		tube = YouTube(url).streams.first().download()
 	except Exception as e:
 		click.echo('An Error has occured :' + str(e))
 
@@ -31,8 +28,19 @@ def createFolder(directory):
 			f.write('After Copying the files in thi folder delete this folder.')
 		if not os.path.exists(directory):
 			os.makedirs(directory)
-	except OSError:
-			click.echo('Error Creating directory '+ str(directory))
+	except Exception as e:
+			click.echo('Error Creating directory '+ str(e))
+
+@main.command()
+@click.argument('url')
+@click.option('--directory', '-d', prompt='Enter the folder Name', help='Directory Name')
+def playlist(url, directory):
+	try:
+		createFolder(directory)
+		tube = Playlist(url).download_all(directory)
+	except Exception as e:
+		click.echo('An Error has occured :' + str(e))
+
 
 
 
