8da9ef4ba0cf988c404fb77fdc5636eac844edf9
Thagana <samuelmthwa79@gmail.com>
2020-09-21 01:06:00 +0200

    utils

---
+++ utils/pathFinder.js
@@ -0,0 +1,7 @@
+// const message = document.querySelector('.info-path');
+// const pathFinder = () => {
+//     const downloadPath = localStorage.getItem('directory');
+//     if(downloadPath){
+//         message.innerHTML = 'Path aleady set'
+//     }
+// }
